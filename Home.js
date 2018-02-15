37ba1b5787bab5345003a83e69b1e5b665eb779e
NerdyTurtle264 <35607947+NerdyTurtle264@users.noreply.github.com>
2018-02-15 10:03:21 -0500

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,35 @@
+function MobileDropFunc() {
+  var x = document.getElementById("MobileNav");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+function MobileAccFunc() {
+    var x = document.getElementById("MobileNavAcc");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-green";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+        x.previousElementSibling.className = 
+        x.previousElementSibling.className.replace(" w3-green", "");
+    }
+}
+function myMap()
+{
+  myCenter=new google.maps.LatLng(41.216065, -79.391593);
+  var mapOptions= {
+    center:myCenter,
+    zoom:12, scrollwheel: true, draggable: true,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMapClarion"),mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: myCenter,
+  });
+  marker.setMap(map);
+}
