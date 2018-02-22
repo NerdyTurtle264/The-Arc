17781d7adad96c3b30b73a04f6bc885bd0b45567
NerdyTurtle264 <35607947+NerdyTurtle264@users.noreply.github.com>
2018-02-22 15:58:22 -0500

    Started implementation of slideshow for header

--- myscripts.js
+++ myscripts.js
@@ -34,3 +34,21 @@ function myMap()
   marker.setMap(map);
 }
 w3.includeHTML();
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("header-slides");
+    if (n > x.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+    }
+    x[slideIndex-1].style.display = "block"; 
+}
